34d3d05b2775a122470eba3aa98b655340d84586
77398 <manu.serpette@ntn-snr.fr>
2022-07-29 12:30:26 +0200

    overflow finished

--- my-app/pages/app/home.js
+++ my-app/pages/app/home.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import ItemList from '../../components/item/itemlist'
 export default function Home() {
   return (
-    <div >
+    <div className='main-home'>
       <Head>
         <title>GRANE</title>
         <meta name="description" content="Grane online app" />
@@ -11,7 +11,8 @@ export default function Home() {
       </Head>
 
       <div className='ItemList'>
-      
+        <div className='home-div-p'>        itemList :
+</div>
         <ItemList></ItemList>
 
     
